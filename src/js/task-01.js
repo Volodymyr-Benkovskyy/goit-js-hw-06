8968ca240ba62c8621eae080f95fefd93887f6e4
Volodymyr-Benkovskyy <123126442+Volodymyr-Benkovskyy@users.noreply.github.com>
2023-03-28 14:11:14 +0200

    update tasks 1-6

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,9 +2,7 @@ const items = document.querySelectorAll(".item");
 
 console.log(`Number of categories: ${items.length}`);
 
-items.forEach((item) => {
-  const itemsTitle = item.querySelector("h2").textContent;
-  const itemsSum = item.querySelectorAll("ul li").length;
-  console.log(`Category: ${itemsTitle}`);
-  console.log(`Elements: ${itemsSum}`);
+items.forEach((el) => {
+  console.log(`Category: ${el.firstElementChild.textContent}`);
+  console.log(`Elements: ${el.lastElementChild.children.length}`);
 });
